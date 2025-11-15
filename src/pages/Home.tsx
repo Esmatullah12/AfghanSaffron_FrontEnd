6f113dc61112e818f88a82cf4124789af68fe7ca
Esmatullah <esmatqadeemi12@gmail.com>
2025-11-15 19:03:46 +0430

    Resolve the error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
-import Hero from "../components/home/Hero"
-import BenefitsSection from "../components/home/BenefitsSection"
-import PRoductShowCase from "../components/home/ProductShowCase"
-import Testimonial from "../components/home/Testimonial"
-import AboutUs from "../components/home/AboutUs"
+import Hero from "../components/Home/Hero"
+import BenefitsSection from "../components/Home/BenefitsSection"
+import PRoductShowCase from "../components/Home/ProductShowCase"
+import Testimonial from "../components/Home/Testimonial"
+import AboutUs from "../components/Home/AboutUs"
 import Layout from "../components/layout/Layout"
 
 const Home = () => {
